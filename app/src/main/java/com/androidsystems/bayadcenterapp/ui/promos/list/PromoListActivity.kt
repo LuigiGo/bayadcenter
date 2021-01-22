a7215ca47efd37745c934bb2433296bd5cf89e19
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 16:44:17 +0800

    * setting up PromoListActivity recyclerview * implementing adapter and functionalities in PromoListActivity recyclerview

--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
@@ -1,27 +1,34 @@
 package com.androidsystems.bayadcenterapp.ui.promos.list
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.view.View.OnClickListener
+import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.androidsystems.bayadcenterapp.R
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.androidsystems.bayadcenterapp.R.layout
 import com.androidsystems.bayadcenterapp.core.base.BaseActivity
 import com.androidsystems.bayadcenterapp.core.utils.Status.SUCCESS
-import com.androidsystems.bayadcenterapp.ui.promos.details.PromoDetailsActivity
-import kotlinx.android.synthetic.main.activity_promo_list.button
-import kotlinx.android.synthetic.main.activity_promo_list.textView
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponse
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponseItem
+import kotlinx.android.synthetic.main.activity_promo_list.rvPromoList
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class PromoListActivity : BaseActivity(), OnClickListener {
+class PromoListActivity : BaseActivity() {
+
+    @Inject
+    lateinit var context: Context
 
     @Inject
     lateinit var mFactory: PromoListViewModelFactory
-
     lateinit var mViewModel: PromoListViewModel
+
+    private lateinit var mAdapter: PromoListAdapter
+
+    var mPromoListData = ArrayList<PromoResponseItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +43,15 @@ class PromoListActivity : BaseActivity(), OnClickListener {
     }
 
     private fun initViews() {
-        button.setOnClickListener(this)
+        mAdapter = PromoListAdapter(context)
+        mAdapter.setData(mPromoListData)
+        val layoutManager = LinearLayoutManager(this, VERTICAL, false)
+        val dividerItemDecoration = DividerItemDecoration(rvPromoList.context, layoutManager.orientation)
+
+        rvPromoList.layoutManager = layoutManager
+        rvPromoList.setHasFixedSize(true)
+        rvPromoList.addItemDecoration(dividerItemDecoration)
+        rvPromoList.adapter = mAdapter
     }
 
     private fun loadPromos() = launch {
@@ -44,22 +59,14 @@ class PromoListActivity : BaseActivity(), OnClickListener {
         promos.observe(this@PromoListActivity, Observer {
             when (it.status) {
                 SUCCESS -> {
-                    textView.text = it.data.toString()
+                    mPromoListData.addAll(it.data as PromoResponse)
+                    mAdapter.notifyDataSetChanged()
+
+                    Log.e("test>>>", mPromoListData.size.toString())
                 }
                 else -> {
                 }
             }
         })
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.button -> {
-                val intent = Intent(this, PromoDetailsActivity::class.java)
-                startActivity(intent)
-            }
-            else -> {
-            }
-        }
     }
 }
