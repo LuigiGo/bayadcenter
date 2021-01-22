6f097e97add7e0732c98b0b24789544b1fb06b93
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 14:30:38 +0800

    test viewmodelfactory implementation in PromoListActivity.kt

--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
@@ -1,13 +1,45 @@
 package com.androidsystems.bayadcenterapp.ui.promos.list
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.androidsystems.bayadcenterapp.R.layout
 import com.androidsystems.bayadcenterapp.core.base.BaseActivity
+import com.androidsystems.bayadcenterapp.core.utils.Status.SUCCESS
+import kotlinx.android.synthetic.main.activity_promo_list.textView
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class PromoListActivity : BaseActivity() {
+
+    @Inject
+    lateinit var mFactory: PromoListViewModelFactory
+
+    lateinit var mViewModel: PromoListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_promo_list)
+
+        mViewModel = ViewModelProvider(this, mFactory).get(PromoListViewModel::class.java)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadPromos()
+    }
+
+    private fun loadPromos() = launch {
+
+        val promos = mViewModel.loadPromoList.await()
+        promos.observe(this@PromoListActivity, Observer {
+            when (it.status) {
+                SUCCESS -> {
+                    textView.text = it.data.toString()
+                }
+                else -> {
+                }
+            }
+        })
     }
 }
