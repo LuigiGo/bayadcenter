e7119a5c6c7945e81b31f6161b46dd1c214e8ab6
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 15:06:21 +0800

    add binding of PromoDetailsActivity in ActivityBindingModule.kt

--- app/src/main/java/com/androidsystems/bayadcenterapp/core/di/module/ActivityBindingModule.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/core/di/module/ActivityBindingModule.kt
@@ -1,5 +1,6 @@
 package com.androidsystems.bayadcenterapp.core.di.module
 
+import com.androidsystems.bayadcenterapp.ui.promos.details.PromoDetailsActivity
 import com.androidsystems.bayadcenterapp.ui.promos.list.PromoListActivity
 import dagger.Module
 import dagger.android.AndroidInjectionModule
@@ -10,4 +11,7 @@ abstract class ActivityBindingModule {
 
     @ContributesAndroidInjector
     abstract fun bindPromoListActivity(): PromoListActivity
+
+    @ContributesAndroidInjector
+    abstract fun bindPromoDetailsActivity(): PromoDetailsActivity
 }
