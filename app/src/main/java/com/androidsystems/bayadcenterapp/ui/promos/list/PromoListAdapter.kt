2b7f714e6b3811b5567ccfee4cbacc8bb2321da2
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 17:25:16 +0800

    implement onitemlistener in PromoListAdapter class

--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListAdapter.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListAdapter.kt
@@ -1,5 +1,6 @@
 package com.androidsystems.bayadcenterapp.ui.promos.list
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -12,18 +13,24 @@ import com.google.android.material.textview.MaterialTextView
 import java.util.ArrayList
 import javax.inject.Inject
 
+interface OnItemClickListener {
+    fun onItemClicked(promoItem: PromoResponseItem)
+}
+
 class PromoListAdapter @Inject constructor(
     private val context: Context
 ) : RecyclerView.Adapter<PromoListAdapter.ViewHolder>() {
 
-    var mData = ArrayList<PromoResponseItem>()
+    private var mData = ArrayList<PromoResponseItem>()
+    private lateinit var mListener: OnItemClickListener
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val imgPromoStatus = v.findViewById<ImageView>(R.id.imgPromoStatus)
+        val ivPromoStatus = v.findViewById<ImageView>(R.id.ivPromoStatus)
         val tvPromoName = v.findViewById<MaterialTextView>(R.id.tvPromoName)
         val tvPromoDetails = v.findViewById<MaterialTextView>(R.id.tvPromoDetails)
     }
 
+    @SuppressLint("InflateParams")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(context).inflate(R.layout.adapter_promo_item, null, false)
         return ViewHolder(v)
@@ -37,12 +44,22 @@ class PromoListAdapter @Inject constructor(
         return mData.size
     }
 
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        mListener = listener
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val promoItem = mData[position]
+
         val isPromoUnread = (promoItem.read != 1)
-        holder.imgPromoStatus.isEnabled = isPromoUnread
-        holder.imgPromoStatus.visibility = if (isPromoUnread) View.VISIBLE else View.GONE
+
+        holder.ivPromoStatus.isEnabled = isPromoUnread
+        holder.ivPromoStatus.visibility = if (isPromoUnread) View.VISIBLE else View.GONE
         holder.tvPromoName.text = promoItem.name
         holder.tvPromoDetails.text = promoItem.details
+
+        holder.itemView.setOnClickListener {
+            mListener.onItemClicked(promoItem)
+        }
     }
 }
