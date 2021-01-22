815835a065f03916f3fec403ed1e05cf15300bea
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 15:03:47 +0800

    add PromoDetailsActivity

---
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/details/PromoDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.androidsystems.bayadcenterapp.ui.promos.details
+
+import android.os.Bundle
+import com.androidsystems.bayadcenterapp.R
+import com.androidsystems.bayadcenterapp.core.base.BaseActivity
+
+class PromoDetailsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promo_details)
+    }
+}
