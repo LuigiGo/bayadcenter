25866a56c600d406f1038e41177af9a3a2972f70
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 11:05:13 +0800

    prepare promo user interface

---
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
@@ -0,0 +1,12 @@
+package com.androidsystems.bayadcenterapp.ui.promos.list
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.androidsystems.bayadcenterapp.R.layout
+
+class PromoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_promo_list)
+    }
+}
