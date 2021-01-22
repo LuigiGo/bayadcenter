255e48514f96b20bdb1f05e5e5341e3e0256c2e1
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 13:00:55 +0800

    test webservices calls in PromoListActivity.kt

--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
@@ -1,12 +1,44 @@
 package com.androidsystems.bayadcenterapp.ui.promos.list
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.androidsystems.bayadcenterapp.R.layout
+import com.androidsystems.bayadcenterapp.core.base.BaseActivity
+import com.androidsystems.bayadcenterapp.core.utils.Status.SUCCESS
+import com.androidsystems.bayadcenterapp.data.network.base.NetworkServiceApi
+import com.androidsystems.bayadcenterapp.data.network.web.promos.PromosDataSourceImpl
+import kotlinx.android.synthetic.main.activity_promo_list.textView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PromoListActivity : AppCompatActivity() {
+class PromoListActivity : BaseActivity() {
+
+//    @Inject
+//    lateinit var networkServiceApi: NetworkServiceApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_promo_list)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+//        val dataSource = PromosDataSourceImpl(networkServiceApi)
+
+//        GlobalScope.launch(Dispatchers.Main) {
+//            val promos = dataSource.getPromos()
+//            dataSource.downloadedPromoList.observe(this@PromoListActivity, androidx.lifecycle.Observer {
+//                when (it.status) {
+//                    SUCCESS -> {
+//                        textView.text = it.data.toString()
+//                    }
+//                    else -> {
+//                    }
+//                }
+//            })
+//
+//        }
     }
 }
