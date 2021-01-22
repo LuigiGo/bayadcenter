de97afa3badc6791b97b3d2b705bdf741a50f486
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 17:26:04 +0800

    implement onitemclicked redirection function in PromoListActivity

--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListActivity.kt
@@ -1,6 +1,7 @@
 package com.androidsystems.bayadcenterapp.ui.promos.list
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
@@ -10,25 +11,29 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.androidsystems.bayadcenterapp.R.layout
 import com.androidsystems.bayadcenterapp.core.base.BaseActivity
+import com.androidsystems.bayadcenterapp.core.utils.INTENT_EXTRAS_PROMO_ITEM
 import com.androidsystems.bayadcenterapp.core.utils.Status.SUCCESS
 import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponse
 import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponseItem
+import com.androidsystems.bayadcenterapp.ui.promos.details.PromoDetailsActivity
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_promo_list.rvPromoList
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class PromoListActivity : BaseActivity() {
+class PromoListActivity : BaseActivity(), OnItemClickListener {
 
     @Inject
     lateinit var context: Context
 
     @Inject
+    lateinit var mAdapter: PromoListAdapter
+
+    @Inject
     lateinit var mFactory: PromoListViewModelFactory
-    lateinit var mViewModel: PromoListViewModel
 
-    private lateinit var mAdapter: PromoListAdapter
-
-    var mPromoListData = ArrayList<PromoResponseItem>()
+    private lateinit var mViewModel: PromoListViewModel
+    private var mPromoListData = ArrayList<PromoResponseItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +44,17 @@ class PromoListActivity : BaseActivity() {
     override fun onStart() {
         super.onStart()
         initViews()
+        initListeners()
         loadPromos()
     }
 
+    private fun initListeners() {
+        mAdapter.setOnItemClickListener(this)
+    }
+
     private fun initViews() {
-        mAdapter = PromoListAdapter(context)
         mAdapter.setData(mPromoListData)
+
         val layoutManager = LinearLayoutManager(this, VERTICAL, false)
         val dividerItemDecoration = DividerItemDecoration(rvPromoList.context, layoutManager.orientation)
 
@@ -68,5 +78,11 @@ class PromoListActivity : BaseActivity() {
                 }
             }
         })
+    }
+
+    override fun onItemClicked(promoItem: PromoResponseItem) {
+        val intent = Intent(this, PromoDetailsActivity::class.java)
+        intent.putExtra(INTENT_EXTRAS_PROMO_ITEM, Gson().toJson(promoItem))
+        startActivity(intent)
     }
 }
