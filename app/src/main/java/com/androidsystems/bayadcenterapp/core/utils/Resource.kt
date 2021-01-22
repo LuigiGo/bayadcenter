f86f05efac0d693994596796e2b7dac71d5c5728
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-22 12:57:00 +0800

    * add helper classes to handle api response statuses

---
+++ app/src/main/java/com/androidsystems/bayadcenterapp/core/utils/Resource.kt
@@ -0,0 +1,23 @@
+package com.androidsystems.bayadcenterapp.core.utils
+
+import com.androidsystems.bayadcenterapp.core.utils.Status.ERROR
+import com.androidsystems.bayadcenterapp.core.utils.Status.LOADING
+import com.androidsystems.bayadcenterapp.core.utils.Status.SUCCESS
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    companion object {
+
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(SUCCESS, data, null)
+        }
+
+        fun <T> error(msg: String, data: T?): Resource<T> {
+            return Resource(ERROR, data, msg)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(LOADING, data, null)
+        }
+    }
+}
---
+++ app/src/main/java/com/androidsystems/bayadcenterapp/core/utils/Status.kt
@@ -0,0 +1,7 @@
+package com.androidsystems.bayadcenterapp.core.utils
+
+enum class Status {
+        SUCCESS,
+        ERROR,
+        LOADING
+    }
