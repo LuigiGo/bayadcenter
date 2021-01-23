5f45b3154cc9906aa8b54b6d376e371e22686fcc
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-23 10:58:19 +0800

    add composite disposable to PromoListViewModelFactory

--- app/src/main/java/com/androidsystems/bayadcenterapp/core/di/module/ViewModelFactoryModule.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/core/di/module/ViewModelFactoryModule.kt
@@ -4,12 +4,16 @@ import com.androidsystems.bayadcenterapp.data.network.repository.Repository
 import com.androidsystems.bayadcenterapp.ui.promos.list.PromoListViewModelFactory
 import dagger.Module
 import dagger.Provides
+import io.reactivex.disposables.CompositeDisposable
 
 @Module
 class ViewModelFactoryModule {
 
     @Provides
-    fun providesPromoListViewModelFactory(repository: Repository): PromoListViewModelFactory {
-        return PromoListViewModelFactory(repository)
+    fun providesPromoListViewModelFactory(
+        repository: Repository,
+        compositeDisposable: CompositeDisposable
+    ): PromoListViewModelFactory {
+        return PromoListViewModelFactory(repository, compositeDisposable)
     }
 }
