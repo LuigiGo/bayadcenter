25c56f993a3bcd22b6d9bd04c8f93b386db304a7
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-23 11:01:07 +0800

    *rename promoitemresponse class *add expose in property fields

--- app/src/main/java/com/androidsystems/bayadcenterapp/data/network/entities/promos/PromoItem.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/data/network/entities/promos/PromoItem.kt
@@ -1,13 +1,19 @@
 package com.androidsystems.bayadcenterapp.data.network.entities.promos
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class PromoResponseItem(
+data class PromoItem(
+    @Expose
     val details: String,
+    @Expose(serialize = false)
     @SerializedName("_id")
     val id: String,
+    @Expose
     @SerializedName("image_url")
     val imageUrl: String,
+    @Expose
     val name: String,
-    val read: Int
+    @Expose
+    var read: Int
 )
