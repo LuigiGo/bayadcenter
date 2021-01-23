86e0a7d02a435d26b872bc0fb66872575c2e9b06
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-23 11:02:53 +0800

    revamp coroutine implementation in PromoDataSourceImpl class

--- app/src/main/java/com/androidsystems/bayadcenterapp/data/network/web/promos/PromosDataSource.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/data/network/web/promos/PromosDataSource.kt
@@ -2,11 +2,19 @@ package com.androidsystems.bayadcenterapp.data.network.web.promos
 
 import androidx.lifecycle.LiveData
 import com.androidsystems.bayadcenterapp.core.utils.Resource
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoItem
 import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponse
 
 interface PromosDataSource {
 
     val downloadedPromoList: LiveData<Resource<PromoResponse>>
+    val updatedPromoItem: LiveData<Resource<PromoItem>>
+    val deletedPromoItem: LiveData<Resource<PromoItem>>
 
-    suspend fun getPromoList()
+    fun getPromoList()
+
+    fun updatePromo(promoItem: PromoItem)
+
+    fun deletePromo(promoItem: PromoItem)
+
 }
--- app/src/main/java/com/androidsystems/bayadcenterapp/data/network/web/promos/PromosDataSourceImpl.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/data/network/web/promos/PromosDataSourceImpl.kt
@@ -5,18 +5,68 @@ import androidx.lifecycle.MutableLiveData
 import com.androidsystems.bayadcenterapp.core.base.BaseDataSource
 import com.androidsystems.bayadcenterapp.core.utils.Resource
 import com.androidsystems.bayadcenterapp.data.network.base.NetworkServiceApi
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoItem
 import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponse
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class PromosDataSourceImpl @Inject constructor(private val networkServiceApi: NetworkServiceApi) : BaseDataSource(),
+class PromosDataSourceImpl @Inject constructor(
+    private val networkServiceApi: NetworkServiceApi,
+    private val compositeDisposable: CompositeDisposable
+) : BaseDataSource(),
     PromosDataSource {
 
     private val _downloadedPromoList = MutableLiveData<Resource<PromoResponse>>()
+    private val _updatedPromoItem = MutableLiveData<Resource<PromoItem>>()
+    private val _deletedPromoItem = MutableLiveData<Resource<PromoItem>>()
+
     override val downloadedPromoList: LiveData<Resource<PromoResponse>>
         get() = _downloadedPromoList
 
-    override suspend fun getPromoList() {
-        val response = getResult { networkServiceApi.getPromos() }
-        _downloadedPromoList.postValue(response)
+    override val updatedPromoItem: LiveData<Resource<PromoItem>>
+        get() = _updatedPromoItem
+
+    override val deletedPromoItem: LiveData<Resource<PromoItem>>
+        get() = _deletedPromoItem
+
+    override fun getPromoList() {
+        compositeDisposable.add(
+            networkServiceApi.getPromos()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    _downloadedPromoList.postValue(Resource.success(it))
+                }, {
+                    _downloadedPromoList.postValue(Resource.error(it.message, null))
+                })
+        )
+    }
+
+    override fun updatePromo(promoItem: PromoItem) {
+        compositeDisposable.add(
+            networkServiceApi.updatePromo(promoItem.id, promoItem)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    _updatedPromoItem.postValue(Resource.success(promoItem))
+                }, {
+                    _updatedPromoItem.postValue(Resource.error(it.message, null))
+                })
+        )
+    }
+
+    override fun deletePromo(promoItem: PromoItem) {
+        compositeDisposable.add(
+            networkServiceApi.deletePromo(promoItem.id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    _deletedPromoItem.postValue(Resource.success(promoItem))
+                }, {
+                    _deletedPromoItem.postValue(Resource.error(it.message, null))
+                })
+        )
     }
 }
