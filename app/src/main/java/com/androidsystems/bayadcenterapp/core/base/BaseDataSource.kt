ff4ce576f3ab6454995e5fafaf8e55e751a3103d
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-23 10:55:27 +0800

    add exception handling

--- app/src/main/java/com/androidsystems/bayadcenterapp/core/base/BaseDataSource.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/core/base/BaseDataSource.kt
@@ -1,7 +1,9 @@
 package com.androidsystems.bayadcenterapp.core.base
 
+import com.androidsystems.bayadcenterapp.core.utils.NoConnectivityException
 import com.androidsystems.bayadcenterapp.core.utils.Resource
 import kotlinx.coroutines.Deferred
+import retrofit2.HttpException
 
 abstract class BaseDataSource {
     protected suspend fun <T> getResult(call: suspend () -> Deferred<T>): Resource<T> {
@@ -10,12 +12,20 @@ abstract class BaseDataSource {
             Resource.success(response)
         } catch (e: Exception) {
             return when (e) {
-//                is NoConnectivityException -> Resource.error("No internet connection", null)
-                else -> Resource.error(
-                    "Unknown Exception",
-                    null
-                )
+                is NoConnectivityException -> Resource.error("No internet connection", null)
+                is HttpException -> Resource.error(parseErrorResponse(e), null)
+                else -> Resource.error(e.message, null)
             }
+        }
+    }
+
+    private fun parseErrorResponse(e: HttpException): String {
+        return when (e.code()) {
+            401 -> "Unauthorized Access"
+            403 -> "Forbidden"
+            404 -> "Not Found"
+            500 -> "Internal Server Error"
+            else -> e.response().errorBody()?.charStream()?.readText().toString()
         }
     }
 }
