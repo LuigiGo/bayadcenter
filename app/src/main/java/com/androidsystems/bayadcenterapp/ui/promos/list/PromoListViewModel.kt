ae87a3b08b2738268416c19f7db86a939c785a79
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-23 11:05:12 +0800

    revamp coroutine implementation in viewm model and factory class

--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListViewModel.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListViewModel.kt
@@ -1,12 +1,43 @@
 package com.androidsystems.bayadcenterapp.ui.promos.list
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.androidsystems.bayadcenterapp.core.utils.lazyDeferred
+import com.androidsystems.bayadcenterapp.core.utils.Resource
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoItem
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponse
 import com.androidsystems.bayadcenterapp.data.network.repository.Repository
+import io.reactivex.disposables.CompositeDisposable
 
-class PromoListViewModel(private val repository: Repository) : ViewModel() {
+class PromoListViewModel(private val repository: Repository, private val compositeDisposable: CompositeDisposable) :
+    ViewModel() {
 
-    val loadPromoList by lazyDeferred {
+
+    fun getDownloadedPromos(): LiveData<Resource<PromoResponse>> {
+        return repository.downloadedPromos
+    }
+
+    fun getUpdatedPromoItem(): LiveData<Resource<PromoItem>> {
+        return repository.updatedPromoItem
+    }
+
+    fun getDeletedPromoItem(): LiveData<Resource<PromoItem>> {
+        return repository.deletedPromoItem
+    }
+
+    fun getPromoList() {
         repository.getPromoList()
+    }
+
+    fun updatePromo(promoItem: PromoItem) {
+        repository.updatePromo(promoItem)
+    }
+
+    fun deletePromo(promoItem: PromoItem) {
+        repository.deletePromo(promoItem)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
     }
 }
--- app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListViewModelFactory.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/ui/promos/list/PromoListViewModelFactory.kt
@@ -3,11 +3,15 @@ package com.androidsystems.bayadcenterapp.ui.promos.list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.androidsystems.bayadcenterapp.data.network.repository.Repository
+import io.reactivex.disposables.CompositeDisposable
 
-class PromoListViewModelFactory(private val repository: Repository) : ViewModelProvider.NewInstanceFactory() {
+class PromoListViewModelFactory(
+    private val repository: Repository,
+    private val compositeDisposable: CompositeDisposable
+) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return PromoListViewModel(repository) as T
+        return PromoListViewModel(repository, compositeDisposable) as T
     }
 }
