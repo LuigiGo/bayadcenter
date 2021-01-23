7865b32f09add38e54ed59ae89c9460923f2b92a
Luigi Go <luigianthonyp.go@pccw.com>
2021-01-23 11:00:14 +0800

    revamp code for coroutine implementation

--- app/src/main/java/com/androidsystems/bayadcenterapp/data/network/base/NetworkServiceApi.kt
+++ app/src/main/java/com/androidsystems/bayadcenterapp/data/network/base/NetworkServiceApi.kt
@@ -1,11 +1,28 @@
 package com.androidsystems.bayadcenterapp.data.network.base
 
+import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoItem
 import com.androidsystems.bayadcenterapp.data.network.entities.promos.PromoResponse
-import kotlinx.coroutines.Deferred
+import io.reactivex.Single
+import okhttp3.ResponseBody
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface NetworkServiceApi {
 
-    @GET(API_PROMO)
-    fun getPromos(): Deferred<PromoResponse>
+    @GET(API_GET_PROMO)
+    fun getPromos(): Single<PromoResponse>
+
+    @PUT(API_UPDATE_DELETE_PROMO)
+    fun updatePromo(
+        @Path("promo_id") promoId: String,
+        @Body promoItem: PromoItem
+    ): Single<ResponseBody>
+
+    @DELETE(API_UPDATE_DELETE_PROMO)
+    fun deletePromo(
+        @Path("promo_id") promoId: String,
+    ): Single<ResponseBody>
 }
